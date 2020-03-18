1edb304a4cb28282252ed0ea93d6042861b2238f
khrustalevDev <54330110+khrustalevDev@users.noreply.github.com>
2020-03-18 14:22:35 +0300

    Aviasales Workshop

---
+++ script.js
@@ -0,0 +1,50 @@
+const formSearch = document.querySelector('.form-search'),
+    inputCitiesFrom = formSearch.querySelector('.input__cities-from'),
+    dropdownCitiesTo = document.querySelector('.dropdown__cities-to'),
+    inputDateDepart = document.querySelector('.input__date-depart'),
+    dropdownCitiesFrom = document.querySelector('.dropdown__cities-from'),
+    inputCitiesTo = document.querySelector('.input__cities-to');
+
+const city = ['Москва', 'Санкт-Петербург', 'Челябинск',
+    'Минск', 'Самара', 'Вроцлав',
+    'Одесса', 'Нижний Новгород', 'Калининград',
+    'Ростов-на-Дону'
+];
+
+const showCity = (input, list) => {
+    list.textContent = '';
+
+    if (input.value !== '') {
+
+
+
+        const filterCity = city.filter((item) => {
+            const itemToLowerCase = item.toLowerCase();
+            return itemToLowerCase.includes(input.value.toLowerCase());
+        });
+
+        filterCity.forEach((item) => {
+            const li = document.createElement('li');
+            li.classList.add('dropdown__city');
+            li.textContent = item;
+            list.append(li);
+
+        });
+
+    }
+};
+
+inputCitiesFrom.addEventListener('input', () => {
+    showCity(inputCitiesFrom, dropdownCitiesFrom);
+});
+
+dropdownCitiesFrom.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target.tagName.toLowerCase() === 'li') {
+        inputCitiesFrom.value = target.textContent;
+        dropdownCitiesFrom.textContent = '';
+    }
+});
+
+/* 1) Повторить все в уроке
+2) Написать функционал для поля "Куда" */
